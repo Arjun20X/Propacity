72b4bc042d01f303538dbb7c9399da5f712d5c6b
Arjun20X <arjunpratapsingh2082004@gmail.com>
2024-12-20 04:11:32 +0530

    update

--- pdf-browser/src/components/PDF.jsx
+++ pdf-browser/src/components/PDF.jsx
@@ -2,7 +2,6 @@ import { NavLink } from "react-router-dom";
 
 export const PDF = ({ pdfData, pdfIndex, onSelectPdfForLink }) => {
   return (
-<<<<<<< HEAD
     <>
       <li className=" flex flex-wrap gap-5  rounded-lg" key={pdfIndex}>
         <div>
@@ -44,33 +43,5 @@ export const PDF = ({ pdfData, pdfIndex, onSelectPdfForLink }) => {
         </div>
       </li>
     </>
-=======
-    <li className="pdf-item rounded-lg" key={pdfIndex}>
-      <img
-        src="https://www.digitalvidya.com/blog/wp-content/uploads/2019/10/download-10.webp"
-        alt="PDF Thumbnail"
-        width={150}
-      />
-      <div className="details flex flex-col gap-6">
-        <p className="font-bold text-3xl">
-          Title: <span className="text-red-700">{pdfData.name}</span>
-        </p>
-        <p className="font-bold text-2xl">
-          Authors: <span className="text-red-700">{pdfData.author}</span>
-        </p>
-        <p className="font-bold text-xl">
-          Published: <span className="text-red-700">{pdfData.published}</span>
-        </p>
-      </div>
-      <NavLink to="/reader">
-        <button
-          className="read-btn text-white bg-blue-700 hover:bg-blue-800 px-5 py-2.5"
-          onClick={() => onSelectPdfForLink(pdfData.link)}
-        >
-          READ
-        </button>
-      </NavLink>
-    </li>
->>>>>>> 7495c2c504f014888ef577d200f7bb7c9da50ffd
   );
 };
